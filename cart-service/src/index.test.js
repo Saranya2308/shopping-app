9601ed4dc0105e3154493ab21774dccee3d12a2e
Saranya2308 <saranyachandu23@gmail.com>
2025-05-08 18:34:07 -0400

    Update index.test.js

--- cart-service/src/index.test.js
+++ cart-service/src/index.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
-const app = require('./index'); // Use relative path
+const app = require('./index');
 
 describe('Cart Service', () => {
-  it('GET / should return status 200 and correct message', async () => {
+  it('GET / should return 200 and the correct message', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Cart Service is running!');
